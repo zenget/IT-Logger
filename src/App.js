b93a57aa8ebf22e137369fcb17dcfa87b7e50c7c
Dawit Getachew <dgetachew@paga.com>
2021-07-12 16:34:02 +0300

    component ui implemented

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Fragment, useEffect } from 'react'
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModel';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+
+const App = () => {
+
+  useEffect(() => {
+    // Initialized Materialize js
+    M.AutoInit()
+  }, [])
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
